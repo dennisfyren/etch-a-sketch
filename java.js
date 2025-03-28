bec4d5243f98355e213cb131519aa7eb7142fa3d
Dennis Fyrén <106529711+dennisfyren@users.noreply.github.com>
2025-03-28 17:56:14 +0100

    Added functionality on draw, finished project.

--- java.js
+++ java.js
@@ -1,7 +1,12 @@
 const mainContent = document.querySelector("#container");
-const userInput = prompt("How many boxes?");
+const userInput = prompt("How many boxes? (max 70)");
 
 function drawBox(userInput) {
+    if (userInput > 70) {
+        alert("Too many, try less");
+        window.location.reload();
+    } else  {
+    
     for (i = 0; i < userInput; i++){
         const rowBox = document.createElement("div");
         rowBox.classList.add("boxes");
@@ -12,10 +17,29 @@ function drawBox(userInput) {
                 rowBox.appendChild(colBox);
             }
     }
-};
+}};
 
 drawBox(userInput);
 
-const box = document.querySelector("div");
+const hoverBox = document.querySelectorAll(".boxes2");
+hoverBox.forEach((hoverBox) => {
+    hoverBox.addEventListener("mouseenter", () => {
+        hoverBox.classList.add("boxes-hover");
+        
+    })
+});
 
-box.onmouseover
+const btn = document.querySelector("button");
+
+btn.addEventListener("click", () => {
+    window.location.reload();
+});
+
+
+btn.addEventListener("mouseenter", () => {
+    btn.classList.add("button-hover");
+})
+
+btn.addEventListener("mouseleave", () => {
+    btn.classList.remove("button-hover");
+})
